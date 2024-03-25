f98a870bf8a8593f3d9b264f5251b29876a176e0
JECSRI <163835686+JECSRI@users.noreply.github.com>
2024-03-25 11:11:45 +0100

    Add files via upload

---
+++ lab05/utils.php
@@ -0,0 +1,13 @@
+<?php
+    //utility function for creating HTML select elements.
+
+    function generateNumericSelect($name,$from,$to){
+        echo "<select name=\"$name\" class=\"form-select\">";
+        for($i=$from; $i <= $to;$i++)
+        {
+            echo "<option value=\"$i\">$i</option>";
+        }
+        echo "</select>";
+    }
+
+?>
